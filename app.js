fa7b0fdcbb111f92fe154ee6b75f0c3db1e3185b
g1st <gintstan@gmail.com>
2017-12-27 20:43:06 +0000

    minor tweaks

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const index = require('./routes/index.js');
 const config = require('./config/secret.js');
-require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({
+  path: 'variables.env'
+});
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect(
+  `mongodb://${process.env.mongodb_user}:${
+    process.env.mongodb_pass
+  }@ds131137.mlab.com:31137/nightlife`, {
+    useMongoClient: true
+  }
+);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
 // create application/x-www-form-urlencoded parser
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 // static files in public directory
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,42 +3,62 @@ const { body, validationResult } = require('express-validator/check');
 const { sanitizeBody } = require('express-validator/filter');
 
 exports.login = (req, res) => {
-  res.render('login', { title: 'Login' });
+  res.render('login', {
+    title: 'Login'
+  });
 };
 
 exports.signup = (req, res) => {
-  res.render('signup', { title: 'Signup' });
+  res.render('signup', {
+    title: 'Signup'
+  });
 };
 
 exports.signMe = (req, res) => {
   const errors = validationResult(req);
+  // console.log(errors)
   if (!errors.isEmpty()) {
     // TODO: flash errors nicely
-    // return res.status(422).json({ errors: errors.mapped() });
-    return res.render('signup', { title: 'Signup', body: req.body });
+    // return res.status(422).json({
+    //   errors: errors.mapped()
+    // });
+    return res.render('signup', {
+      title: 'Signup',
+      body: req.body
+    });
   }
   // all good
+
   res.redirect('/');
 };
 
 exports.loginValidator = [
   body('email', 'Email address is not valid').isEmail(),
   body('password')
-    .isLength({ min: 8, max: undefined })
+    .isLength({
+      min: 8,
+      max: undefined
+    })
     .withMessage('Password must be at least 8 characters long.')
 ];
 
 exports.signupValidator = [
   body('email', 'Email address is not valid').isEmail(),
-  body('name', 'Name must contain at least 2 characters.')
+  body('name', 'Must provide a name.')
     .not()
     .isEmpty()
     .trim(),
   body('password')
-    .isLength({ min: 8, max: undefined })
+    .isLength({
+      min: 8,
+      max: undefined
+    })
     .withMessage('Password must be at least 8 characters long.'),
   body('password-confirm')
-    .isLength({ min: 8, max: undefined })
+    .isLength({
+      min: 8,
+      max: undefined
+    })
     .withMessage('Confirmed password must be at least 8 characters long.'),
   sanitizeBody('email').normalizeEmail({
     remove_dots: false,
